24f940a00a314cbdd5c14cb8bff6830efe5414c5
mmccauley <44855113+MelissaMcCauley@users.noreply.github.com>
2021-05-07 16:09:49 -0400

    committing unstaged changes so that email can be corrected from 'fakeemail@fakeemail.com to '44855113+MelissaMcCauley@users.noreply.github.com' in the git history (to prevent the wrong user from being assigned as the contributer on github simply for using the same dummy email address

--- mealmageapp/models.py
+++ mealmageapp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 class StoredDish(models.Model):
 	"""A particular dish that has been added by the user to their account"""
 	DISH_TYPE_CHOICES = [
